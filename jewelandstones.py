318575e55076a0b0ab81fe3f2592b0bf13f59c39
Samanway Chakraborty <43331341+sampoprock@users.noreply.github.com>
2020-05-03 11:14:01 +0530

    Add files via upload

---
+++ jewelandstones.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        return sum([s in J for s in S])
